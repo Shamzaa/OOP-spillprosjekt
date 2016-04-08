664315094e4efeaaaca6d63e0149dd497ad6450e
Jarle Trollebø <aborysa@gmail.com>
2016-04-08 18:14:54 +0200

    Changed compare to make items with higher depth to sort to lower index

--- src/game/graphics/DrawableCompare.java
+++ src/game/graphics/DrawableCompare.java
@@ -6,7 +6,7 @@ public class DrawableCompare implements Comparator<Drawable>{
 
 	@Override
 	public int compare(Drawable o1, Drawable o2) {
-		return (o1.getDepth() - o2.getDepth()) < 0 ? -1 : 0;
+		return (o1.getDepth() - o2.getDepth()) < 0 ? 0 : -1;
 	}
 
 }
