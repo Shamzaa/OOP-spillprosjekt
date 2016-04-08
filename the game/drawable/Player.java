85fddc7f2c5da045e3a854478e663c29b4710fc3
Erling Ihlen <erling_pondus@hotmail.com>
2016-04-08 13:35:15 +0200

    Basics for drawable

---
+++ the game/drawable/Player.java
@@ -0,0 +1,36 @@
+package drawable;
+
+public class Player extends Entity{
+	Player(int x, int y){
+		position = new Vector(x, y);
+		// TODO: current sprite, other info loaded from file?
+	}
+	
+	public void move(int x, int y){
+		// will get an x and y value to move, depending on buttons pushed in main program
+		// only w: x = 0, y = 1;
+		// a + d: x = 0, y = 0;
+		// summed up: a sum for each x and y depending on which buttons are down
+		
+		/*
+		 	TODO: calculate how much Player will move for delta time
+		 	and return x and y values for .setPosition
+		 */
+		
+		position.setPosition(x, y);
+		// calculate z value
+		
+		if(x > 0){
+			// turn sprite to right
+		}else if(x < 0){
+			// turn sprite to left
+		}else{
+			if(y > 0){
+				// turn sprite down
+			}else if(y < 0){
+				// turn sprite up
+			}
+			// don't change sprite
+		}
+	}
+}
---
+++ the game/drawable/Vector.java
@@ -0,0 +1,37 @@
+package drawable;
+
+public class Vector {
+	// {x, y}
+	private Integer[] position = new Integer[3];
+	
+	Vector(int x, int y){
+		position[0] = x;
+		position[1] = y;
+		
+		// calculate z-value (position[2])
+		// no need for validation?
+	}
+	
+	// TODO: make movement relevant? 
+	public void setPosition(int x, int y){
+		position[0] = x;
+		position[1] = y;
+		
+		// calculate z-position
+	}
+	/*
+	public Integer[] getPosition(){
+		return position;
+	}
+	*/
+	// or getX, getY, getZ?
+	public int getX(){
+		return position[0];
+	}
+	public int getY(){
+		return position[1];
+	}
+	public int getZ(){
+		return position[2];
+	}
+}
