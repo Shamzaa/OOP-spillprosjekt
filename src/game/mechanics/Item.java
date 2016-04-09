82538a7331d7d45a709bd24a88e62bf3b0d6a293
Jarle Trollebø <aborysa@gmail.com>
2016-04-09 17:41:12 +0200

    Fleshed out a few things

--- src/game/mechanics/Item.java
+++ src/game/mechanics/Item.java
@@ -1,18 +1,46 @@
 package game.mechanics;
 
+import java.util.HashMap;
+
 public class Item {
 	private String name;
+	private boolean stackable;
+	private String itemId;
+	private boolean dead = false;
 	// some sort of pointer to a file for information
-	
-	public Item(String name){
-		this.name = name;
+	private static HashMap<String,Item> itemMap = new HashMap<String,Item>();
+	public Item(String name,String itemId){
+		if(!itemMap.containsKey(itemId)){
+			this.name = name;
+			this.itemId = itemId;
+			itemMap.put(itemId, this);
+		}else{
+			throw new IllegalArgumentException("Duplicate key!\nTwo items can't share itemId");
+		}
 	}
-	
+	public Item(Item i){
+		//Creates a new "unregistered"/unique item
+		//Can not be stacked at all
+		this.name = i.name;
+		this.itemId = i.itemId;
+		this.stackable = false;
+	}
+	public Item(String itemId){
+		this(itemMap.get(itemId));
+	}
+	public static Item getItemById(String itemId){
+		if(itemMap.containsKey(itemId)){
+			return itemMap.get(itemId);
+		}		
+		throw new IllegalArgumentException("No item with id " + itemId + " exists!");
+	}
 	public String getName(){
 		return name;
 	}
-	
-	public void setName(String name){
-		this.name = name;
+	public void kill(){
+		dead = true;
+	}
+	public boolean isStackable(){
+		return stackable;
 	}
 }
